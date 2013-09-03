a1b2f239f1789b70d5a2a78d38338037a183c79c
Kai Yuan <kent.yuan@Gmail.com>
2013-09-03 21:32:12 +0200

    clear template before git archive

--- getWikiPages.sh
+++ getWikiPages.sh
@@ -4,6 +4,7 @@ TEMPLATE_REPO=~/MyStuff/myCodes/vim/wiki_template
 WIKI_HTML_DIR=~/vimwiki_html
 rsync -av --no-links $WIKI_HTML_DIR/* . 
 cd $TEMPLATE_REPO
+rm -rf template
 mkdir -p $MY_DIR/template
 git archive master|tar -x -C "$MY_DIR/template"
 cd $MY_DIR
